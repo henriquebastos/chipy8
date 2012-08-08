6974cba56413527c8b7cef9e4b6ad6ca9fe5049e
Henrique Bastos <henrique@bastos.net>
2012-08-08 01:40:08 -0300

    Clarify values used in tests.

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -10,13 +10,13 @@ class TestMemory(TestCase):
     def test_write(self):
         'Write a byte to memory then read it.'
         address = 0x200
-        self.memory.write_byte(0x200, 0x01)
-        self.assertEqual(0x01, self.memory.read_byte(0x200))
+        self.memory.write_byte(address, 0x01)
+        self.assertEqual(0x01, self.memory.read_byte(address))
 
     def test_load(self):
         'Load a stream of bytes to memory starting on an address.'
         address = 0x200
-        self.memory.load(0x200, [0x01, 0x02, 0x03])
+        self.memory.load(address, [0x01, 0x02, 0x03])
         self.assertEqual(0x01, self.memory.read_byte(address))
         self.assertEqual(0x02, self.memory.read_byte(address + 1))
         self.assertEqual(0x03, self.memory.read_byte(address + 2))
