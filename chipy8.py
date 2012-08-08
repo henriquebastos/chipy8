621bddb1e0b16e486f5175644b14a8663f7af66e
Henrique Bastos <henrique@bastos.net>
2012-08-08 00:52:30 -0300

    Initial Chip8 architecture description

---
+++ chipy8.py
@@ -0,0 +1,20 @@
+
+
+class Chip8(object):
+    def __init__(self):
+        self.registers = [0x00] * 16
+        self.index_register = 0
+        self.program_counter = 0
+
+        self.memory = [0x00] * 4096
+        # 0x000-0x1FF - Chip 8 interpreter (contains font set in emu)
+        # 0x050-0x0A0 - Used for the built in 4x5 pixel font set (0-F)
+        # 0x200-0xFFF - Program ROM and work RAM
+
+        self.screen = [0x00] * 32 * 64
+        self.keyboard = [0x00] * 16
+        self.stack = []
+
+        self.delay_timer = 0
+        self.sound_timer = 0
+
---
+++ tests.py
@@ -0,0 +1,51 @@
+from unittest import TestCase, main
+from chipy8 import Chip8
+
+
+class Chip8Architecture(TestCase):
+    def setUp(self):
+        self.cpu = Chip8()
+
+    def test_memory_length(self):
+        'Chip8 has 4096 bytes of memory.'
+        self.assertEqual(4096, len(self.cpu.memory))
+
+    def test_memory_clear(self):
+        'Chip8 must be initialized with a clear memory.'
+        self.assertListEqual([0x00] * 4096, self.cpu.memory)
+
+    def test_register_count(self):
+        'Chip8 has 16 registers.'
+        self.assertEqual(16, len(self.cpu.registers))
+
+    def test_program_counter(self):
+        'Chip8 has a program counter.'
+        self.assertEqual(0, self.cpu.program_counter)
+
+    def test_index_register(self):
+        'Chip8 has an index register.'
+        self.assertEqual(0, self.cpu.index_register)
+
+    def test_screen(self):
+        'Chip8 has a 64 * 32 screen (2048 pixels).'
+        self.assertEqual(2048, len(self.cpu.screen))
+
+    def test_delay_timer(self):
+        'Chip8 has a delay timer that counts to 0 at 60Hz.'
+        self.assertEqual(0, self.cpu.delay_timer)
+
+    def test_sound_timer(self):
+        'Chip8 has a count down sound timer that beeps on non-zero value.'
+        self.assertEqual(0, self.cpu.sound_timer)
+
+    def test_stack(self):
+        'Chip8 has a stack to return from jumps and calls.'
+        self.assertIsInstance(self.cpu.stack, list)
+
+    def test_keyboard(self):
+        'Chip8 has a hex keyboard with 16 keys.'
+        self.assertEqual(16, len(self.cpu.keyboard))
+
+
+if __name__ == '__main__':
+    main()
