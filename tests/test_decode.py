d1610f6054daa58f5cf139a4ce1db32ca0a9e1e6
Henrique Bastos <henrique@bastos.net>
2012-08-10 13:02:24 -0300

    Rearrange decode tests to ease comparison to the manual

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -7,7 +7,8 @@ class TestOpcodeDecode(TestCase):
     def setUp(self):
         self.cpu = Chip8()
 
-    # Opcodes with no arguments
+    def test_0NNN(self):
+        self.assertEqual(self.cpu.decode(0x0200), (0x0, 0x200))
 
     def test_00E0(self):
         self.assertEqual(self.cpu.decode(0x00E0), (0x00E0,))
@@ -15,24 +16,11 @@ class TestOpcodeDecode(TestCase):
     def test_00EE(self):
         self.assertEqual(self.cpu.decode(0x00EE), (0x00EE,))
 
-    # Opcodes with one argument: memory address
-
-    def test_0NNN(self):
-        self.assertEqual(self.cpu.decode(0x0200), (0x0, 0x200))
-
     def test_1NNN(self):
         self.assertEqual(self.cpu.decode(0x1200), (0x1, 0x200))
 
     def test_2NNN(self):
         self.assertEqual(self.cpu.decode(0x2200), (0x2, 0x200))
-
-    def test_ANNN(self):
-        self.assertEqual(self.cpu.decode(0xA200), (0xA, 0x200))
-
-    def test_BNNN(self):
-        self.assertEqual(self.cpu.decode(0xB200), (0xB, 0x200))
-
-    # Opcodes with 2 arguments: one register and a constant word.
 
     def test_3XNN(self):
         self.assertEqual(self.cpu.decode(0x3199), (0x3, 0x1, 0x99))
@@ -40,16 +28,14 @@ class TestOpcodeDecode(TestCase):
     def test_4XNN(self):
         self.assertEqual(self.cpu.decode(0x4199), (0x4, 0x1, 0x99))
 
+    def test_5XY0(self):
+        self.assertEqual(self.cpu.decode(0x5120), (0x5, 0x1, 0x2))
+
     def test_6XNN(self):
         self.assertEqual(self.cpu.decode(0x6199), (0x6, 0x1, 0x99))
 
     def test_7XNN(self):
         self.assertEqual(self.cpu.decode(0x7199), (0x7, 0x1, 0x99))
-
-    # Opcodes with 2 register arguments
-
-    def test_5XY0(self):
-        self.assertEqual(self.cpu.decode(0x5120), (0x5, 0x1, 0x2))
 
     def test_8XY0(self):
         self.assertEqual(self.cpu.decode(0x8120), (0x8000, 0x1, 0x2))
@@ -80,3 +66,9 @@ class TestOpcodeDecode(TestCase):
 
     def test_9XY0(self):
         self.assertEqual(self.cpu.decode(0x9120), (0x9000, 0x1, 0x2))
+
+    def test_ANNN(self):
+        self.assertEqual(self.cpu.decode(0xA200), (0xA, 0x200))
+
+    def test_BNNN(self):
+        self.assertEqual(self.cpu.decode(0xB200), (0xB, 0x200))
