45050371f602f095566428696c48bc2e7954d045
Henrique Bastos <henrique@bastos.net>
2012-08-11 21:26:07 -0300

    Refactor instruction tests to simplify the CPU state setup.

--- tests/test_instructions.py
+++ tests/test_instructions.py
@@ -7,87 +7,76 @@ class TestInstructios(TestCase):
     def setUp(self):
         self.cpu = Chip8()
 
+    def registers(self, **registers):
+        for r, datum in registers.items():
+            self.cpu.registers[int(r[-1])] = datum
+
+    def execute(self, op, at=ENTRY_POINT):
+        data = [op >> 8, ((op | 0xFF00) ^ 0xFF00)]
+        self.cpu.memory.load(at, data)
+        self.cpu.program_counter = at
+        self.cpu.cycle()
+
     def test_00EE(self):
         self.cpu.stack.append(0x200)
-        self.cpu.memory.load(0x400, [0x00, 0xEE])
-        self.cpu.program_counter = 0x400
-        self.cpu.cycle()
+        self.execute(0x00EE, at=0x400)
         self.assertListEqual([], self.cpu.stack)
         self.assertEqual(0x200, self.cpu.program_counter)
 
     def test_1NNN(self):
-        self.cpu.memory.load(ENTRY_POINT, [0x14, 0x00])
-        self.cpu.cycle()
+        self.execute(0x1400)
         self.assertEqual(0x400, self.cpu.program_counter)
 
     def test_2NNN(self):
-        self.cpu.memory.load(ENTRY_POINT, [0x24, 0x00])
-        self.cpu.cycle()
+        self.execute(0x2400)
         self.assertEqual(0x400, self.cpu.program_counter)
         self.assertListEqual([0x200], self.cpu.stack)
 
     def test_3XNN_equal(self):
-        value = 0x42
-        self.cpu.registers[0x5] = value
-        self.cpu.memory.load(ENTRY_POINT, [0x35, value])
-        self.cpu.cycle()
+        self.registers(V5=0x42)
+        self.execute(0x3542)
         self.assertEqual(ENTRY_POINT+0x4, self.cpu.program_counter)
 
     def test_3XNN_unequal(self):
-        valueA, valueB = 0x42, 0x24
-        self.cpu.registers[0x5] = valueA
-        self.cpu.memory.load(ENTRY_POINT, [0x35, valueB])
-        self.cpu.cycle()
+        self.registers(V5=0x42)
+        self.execute(0x3524)
         self.assertEqual(ENTRY_POINT+0x2, self.cpu.program_counter)
 
     def test_4XNN_equal(self):
-        value = 0x69
-        self.cpu.registers[0x2] = value
-        self.cpu.memory.load(ENTRY_POINT, [0x42, value])
-        self.cpu.cycle()
+        self.registers(V2=0x69)
+        self.execute(0x4269)
         self.assertEqual(ENTRY_POINT+0x2, self.cpu.program_counter)
 
     def test_4XNN_unequal(self):
-        valueA, valueB = 0x69, 0x70
-        self.cpu.registers[0x2] = valueA
-        self.cpu.memory.load(ENTRY_POINT, [0x42, valueB])
-        self.cpu.cycle()
+        self.registers(V2=0x69)
+        self.execute(0x4270)
         self.assertEqual(ENTRY_POINT+0x4, self.cpu.program_counter)
 
     def test_5XY0_equal(self):
-        value = 0x42
-        self.cpu.registers[0x2] = value
-        self.cpu.registers[0x3] = value
-        self.cpu.memory.load(ENTRY_POINT, [0x52, 0x30])
-        self.cpu.cycle()
+        self.registers(V2=0x42, V3=0x42)
+        self.execute(0x5230)
         self.assertEqual(ENTRY_POINT+0x4, self.cpu.program_counter)
 
     def test_5XY0_unequal(self):
-        self.cpu.registers[0x2] = 0x42
-        self.cpu.registers[0x3] = 0x24
-        self.cpu.memory.load(ENTRY_POINT, [0x52, 0x30])
-        self.cpu.cycle()
+        self.registers(V2=0x42, V3=0x24)
+        self.execute(0x5230)
         self.assertEqual(ENTRY_POINT+0x2, self.cpu.program_counter)
 
     def test_8XY0(self):
-        X, Y = 0x2, 0x4
-        self.cpu.registers[Y] = 0x42
-        self.cpu.memory.load(ENTRY_POINT, [0x82, 0x40])
-        self.cpu.cycle()
-        self.assertEqual(self.cpu.registers[X],
-                         self.cpu.registers[Y])
+        self.registers(V2=0x00, V4=0x42)
+        self.execute(0x8240)
+        self.assertEqual(self.cpu.registers[2],
+                         self.cpu.registers[4])
         self.assertEqual(ENTRY_POINT+0x2, self.cpu.program_counter)
 
     def test_ANNN(self):
-        self.cpu.memory.load(ENTRY_POINT, [0xA4, 0x00])
-        self.cpu.cycle()
+        self.execute(0xA400)
         self.assertEqual(0x400, self.cpu.index_register)
         self.assertEqual(ENTRY_POINT+0x2, self.cpu.program_counter)
 
     def test_FX55(self):
         self.cpu.index_register = 0x400
-        self.cpu.memory.load(ENTRY_POINT, [0xF5, 0x55])
-        self.cpu.cycle()
+        self.execute(0xF555)
         registers = self.cpu.registers[:5+1]
         in_memory = self.cpu.memory.read(0x400, len(registers))
         self.assertEqual(registers, in_memory)
