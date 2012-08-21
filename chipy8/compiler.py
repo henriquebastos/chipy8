b61bd916f924c0ad38e6ae35000056ea2b2518a1
Guto Maia <guto@guto.net>
2012-08-21 20:59:00 -0300

    stubbing the syntax analiser. #OnWayCanada

--- chipy8/compiler.py
+++ chipy8/compiler.py
@@ -5,15 +5,15 @@ from analyzer import analyse
 chip8_tokens = [
     dict(type='T_INSTRUCTION', regex=r'^(RCA|DRW|LD|CALL|DT|SE|JP)\s', store=True),    
     dict(type='T_ADDRESS', regex=r'\$([\dA-F]{2,4})', store=True),
-    dict(type='T_HEX_NUMBER', regex=r'\#\$?([\dA-F]{2})', store=True), #TODO: change to HEX_NUMBER
-    dict(type='T_LABEL', regex=r'^([a-zA-Z][_a-zA-Z\d]{1,})\:', store=True),
-    dict(type='T_MARKER', regex=r'^[a-zA-Z][_a-zA-Z\d]{1,}', store=True),
-    dict(type='T_STRING', regex=r'^"[^"]*"', store=True),
+    dict(type='T_HEX_NUMBER', regex=r'\#\$?([\dA-F]{2})', store=True),
     dict(type='T_INDEX_REGISTER', regex=r'^(I)[\s,\]]', store=True),
     dict(type='T_BDC_REGISTER', regex=r'^(B)[\s,]', store=True),
     dict(type='T_F_REGISTER', regex=r'^(F)[\s,]', store=True),
     dict(type='T_REGISTER', regex=r'^(V([\dA-F]))', store=True),
     dict(type='T_SEPARATOR', regex=r'^,', store=True),
+    dict(type='T_LABEL', regex=r'^([a-zA-Z][_a-zA-Z\d]{1,})\:', store=True),
+    dict(type='T_MARKER', regex=r'^[a-zA-Z][_a-zA-Z\d]{1,}', store=True),
+    dict(type='T_STRING', regex=r'^"[^"]*"', store=True),
     dict(type='T_OPEN', regex=r'^\(', store=True),
     dict(type='T_CLOSE', regex=r'^\)', store=True),
     dict(type='T_OPEN_SQUARE_BRACKETS', regex=r'^\[', store=True),
@@ -26,3 +26,6 @@ chip8_tokens = [
 
 def lexical(code):
     return analyse(code, chip8_tokens)
+
+def syntax(tokens):
+    return [dict(type="S_XNN")]
---
+++ chipy8/tests/ld_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+LD Load Instruction Test
+
+With compared with the binary file at the end. It equals to the MOV.
+'''
+import unittest
+from chipy8.compiler import lexical, syntax, semantic
+
+class LdTest(unittest.TestCase):
+
+
+    def test_ld_with_register_and_two(self):
+        tokens = lexical('LD VA, 2')
+        self.assertEquals(4, len(tokens))
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_REGISTER', tokens[1]['type'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_XNN', ast[0]['type'])
