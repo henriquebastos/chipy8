9c1ee5fc93444456bcfc2dbc2cf857886fd1ec81
Guto Maia <guto@guto.net>
2012-08-21 22:32:47 -0300

    incrementally increase compiler accurace as pong instructions is being understood on syntax and semantic compiler... Its like a dojo

--- chipy8/tests/pong_test.py
+++ chipy8/tests/pong_test.py
@@ -3,18 +3,26 @@
 import unittest
 
 import pynes
-from chipy8.compiler import lexical #, syntax, semantic
+from chipy8.compiler import lexical, syntax, semantic
 
 class PongTest(unittest.TestCase):
+
 
     def test_asm_compiler(self):
         f = open ('fixtures/PONG.SRC')
         code = f.read()
         f.close()
         tokens = lexical(code)
+        ast = syntax(tokens)
+        opcodes = semantic(ast)
+        bin = ''.join([chr(opcode) for opcode in opcodes])
+        f = open('fixtures/PONG', 'rb')
+        content = f.read()
+        c = content[:len(bin)]
+        self.assertEquals(c,bin)
+
 
     def test_fixbug_with_marker_dt_loop(self):
         code = 'DT_loop'
         tokens = lexical(code)
         self.assertEquals(1, len(tokens))
-        
