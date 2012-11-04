d959a7007e14a9820757f20e88b1a3ab61e4a3e0
Henrique Bastos <henrique@bastos.net>
2012-11-03 23:39:44 -0200

    Generalize ADDRESS to avoid magic number.

--- chipy8/tests/test_memory.py
+++ chipy8/tests/test_memory.py
@@ -3,38 +3,37 @@ from unittest import TestCase
 from chipy8 import Memory
 
 
+ADDRESS = 0x200
+
+
 class TestMemory(TestCase):
     def setUp(self):
         self.memory = Memory()
 
     def test_write(self):
         'Write a byte to memory then read it.'
-        address = 0x200
-        self.memory.write_byte(address, 0x01)
-        self.assertEqual(0x01, self.memory.read_byte(address))
+        self.memory.write_byte(ADDRESS, 0x01)
+        self.assertEqual(0x01, self.memory.read_byte(ADDRESS))
 
     def test_write_must_receive_byte(self):
         self.assertRaises(ValueError,
-                          self.memory.write_byte, 0x200, 0x100)
+                          self.memory.write_byte, ADDRESS, 0x100)
 
     def test_load_must_receive_list_of_bytes(self):
         self.assertRaises(ValueError,
-                          self.memory.load, 0x200, [0x100])
+                          self.memory.load, ADDRESS, [0x100])
 
     def test_load(self):
         'Load a stream of bytes to memory starting on an address.'
-        address = 0x200
-        self.memory.load(address, [0x01, 0x02, 0x03])
-        self.assertEqual(0x01, self.memory.read_byte(address))
-        self.assertEqual(0x02, self.memory.read_byte(address + 1))
-        self.assertEqual(0x03, self.memory.read_byte(address + 2))
+        self.memory.load(ADDRESS, [0x01, 0x02, 0x03])
+        self.assertEqual(0x01, self.memory.read_byte(ADDRESS))
+        self.assertEqual(0x02, self.memory.read_byte(ADDRESS + 1))
+        self.assertEqual(0x03, self.memory.read_byte(ADDRESS + 2))
 
     def test_read_word(self):
-        address = 0x200
-        self.memory.load(address, [0x01, 0x02])
-        self.assertEqual(0x0102, self.memory.read_word(address))
+        self.memory.load(ADDRESS, [0x01, 0x02])
+        self.assertEqual(0x0102, self.memory.read_word(ADDRESS))
 
     def test_read(self):
-        address = 0x200
-        self.memory.load(address, [0x01, 0x02])
-        self.assertListEqual([0x01, 0x02], self.memory.read(address, 2))
+        self.memory.load(ADDRESS, [0x01, 0x02])
+        self.assertListEqual([0x01, 0x02], self.memory.read(ADDRESS, 2))
